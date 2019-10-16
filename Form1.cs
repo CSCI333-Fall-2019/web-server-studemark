d8945cc1b799f886184d3f5430eaabb555101bd1
studemark <studemark@gmail.com>
2019-10-15 22:54:25 -0500

    Added README and Form1

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebServer {
+   public partial class Form1 : Form {
+      public Form1() {
+         InitializeComponent();
+
+      }
+
+      private void Button1_Click(object sender, EventArgs e) {
+         button1.Enabled = false;
+         button2.Enabled = true;
+         System.Threading.ThreadPool.QueueUserWorkItem(StartWebListener, this);
+      }
+      private void Button2_Click(object sender, EventArgs e) {
+         button2.Enabled = false;
+         button1.Enabled = true;
+      }
+
+      public void StartWebListener(Object obj) {
+         Form1 lForm = (Form1)obj;
+      }
+     
+   }
+}
